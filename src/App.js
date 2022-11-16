cc941d7ef0485d9db2bd3b26fea1da4855b61afb
MY PC <dipesh.sankhala@zanjo.io>
2022-11-16 16:31:24 +0530

    code updating

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Slider from './Slider'
+import Pictures from './Pictures';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+     <Slider/>
+    <Pictures/>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default App;
